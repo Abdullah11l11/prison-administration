2698f5e55b214bc0cbb52f1e1fb136ae0e717401
Abdallah Mazketli <abdallahmazketli244@gmail.com>
2026-01-02 13:20:01 +0300

    feat: delete current project

--- src/pages/login.page.tsx
+++
@@ -1,7 +0,0 @@
-import LoginForm from "@/components/sections/auth/login-form.component";
-
-const LoginPage = () => {
-  return <LoginForm />;
-};
-
-export default LoginPage;
--- src/schema/login.schema.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-export const loginSchema = z.object({
-  username: z.string().min(1, "اسم المستخدم مطلوب"),
-  password: z.string().min(1, "كلمة المرور مطلوبة"),
-});
-
-export type LoginBody = z.infer<typeof loginSchema>;
